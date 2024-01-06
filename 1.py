cf7cfb749d02d5e46efeb81d53205b43cda549f2
tesetgye <147240224+tesetgye@users.noreply.github.com>
2024-01-06 10:04:59 +0900

    Add files via upload

--- 1.py
+++ 1.py
@@ -4,17 +4,15 @@ from streamlit_extras.let_it_rain import rain
 import requests
 import json
 
-x = 0
-
-st.set_page_config(page_title="Snowflakes",)
+st.set_page_config(page_title="NewYear",)
 
 def load_lottie_json(file_path):
     with open(file_path, "r") as file:
         return json.load(file)
 
-def main1():
+def main():
     # 로컬에 저장된 Lottie JSON 파일 경로
-    lottie_file_path = "2.json"  # 여기에 다운로드한 파일 경로를 입력해주세요
+    lottie_file_path = "1.json"  # 여기에 다운로드한 파일 경로를 입력해주세요
 
     # Lottie 파일 로드
     lottie_json = load_lottie_json(lottie_file_path)
@@ -22,17 +20,7 @@ def main1():
     # Streamlit에 Lottie 애니메이션 표시
     st_lottie(lottie_json, speed=1, width=600, height=600)
 
-def main2():
-    # 로컬에 저장된 Lottie JSON 파일 경로
-    lottie_file_path = "3.json"  # 여기에 다운로드한 파일 경로를 입력해주세요
-
-    # Lottie 파일 로드
-    lottie_json = load_lottie_json(lottie_file_path)
-
-    # Streamlit에 Lottie 애니메이션 표시
-    st_lottie(lottie_json, speed=1, width=600, height=600)
-
-def example1():
+def snow():
     rain(
         emoji="❄️",
         font_size=36,
@@ -41,6 +29,6 @@ def example1():
     )
 
 if __name__ == "__main__":
-    main2()
+    main()
 
-example1()
+snow()
